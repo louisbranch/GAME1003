418a7d7d8a2436b6a5ff62c11daf4eae36f65ed2
Luiz Branco <me@luizbranco.com>
2014-11-15 05:13:52 -0500

    Finish assignment 2

--- assignment2/main.js
+++ assignment2/main.js
@@ -14,112 +14,93 @@
       new Hand({id: "player", deck: decks[0], maxCards: 2, playable: true}),
       new Hand({id: "dealer", deck: decks[1]})
     ];
+    this.setEls();
+    show(this.menuEl);
+    hide(this.endEl);
   }
 
+  // Set DOM elements for game and bind events
+  Game.prototype.setEls = function () {
+    this.msgEl = find("win-msg");
+    this.menuEl = find("game-menu");
+    this.stageEl = find("game-stage");
+    this.endEl = find("game-end");
+    var start = find("game-start");
+    var restart = find("game-restart");
+
+    start.onclick = this.start.bind(this);
+    restart.onclick = function () {
+      var game = new Game();
+      game.start();
+    };
+
+    hide(this.msgEl);
+  };
+
+  // Plays start music and enter play loop
   Game.prototype.start = function () {
-    playAudio("battle", .3);
+    hide(this.menuEl);
+    show(this.stageEl);
     this.play();
   };
 
+  // Create a new match until the player doesn't have more cards
   Game.prototype.play = function () {
     var game = this;
     var match = new Match(this.hands);
     match.play(function (winner) {
+      game.displayWinner(winner);
       winner.increaseScore();
       if (winner.hasCards()) game.play();
       else game.end();
     });
   }
 
-  Game.prototype.end = function () {
-    stopAudio("battle");
-    playAudio("ending", .3);
-  };
-
-  function Hand(options) {
-    this.id = options.id;
-    this.deck = options.deck;
-    this.maxCards = options.maxCards || 1;
-    this.playable = options.playable;
-    this.score = 0;
-    this.cards = null;
-    this.selected = null;
-    this.setEls();
-  }
-
-  // Set current match
-  Hand.prototype.setMatch = function (match) {
-    this.match = match;
-  };
-
-  // Whether hand still has cards to play
-  Hand.prototype.hasCards = function () {
-    return !!this.deck.cards.length;
-  };
-
-  // Set hand dom element
-  Hand.prototype.setEls = function () {
-    var id = this.id;
-    this.el = document.getElementById(id + "-hand");
-    this.scoreEl = document.getElementById(id + "-score");
-    if (!this.playable) return;
-    var btn = document.getElementById(id + "-btn");
-    btn.addEventListener("click", this.play.bind(this));
-  };
-
-  // Render max cards for hand and set current cards
-  Hand.prototype.render = function () {
-    this.selected = null;
-    this.el.innerHTML = "";
-    var cards = this.deck.deal(this.maxCards);
-    for (var i = 0, l = cards.length; i < l; i ++) {
-      var card = cards[i];
-      card.setHand(this);
-      this.el.appendChild(card.render(this.playable));
+  // Display match winner
+  Game.prototype.displayWinner = function (winner) {
+    var el = this.msgEl;
+    show(el);
+    el.textContent = winner.id + " wins the hand!"
+    if (winner.id === "player") {
+      el.className = "alert alert-success";
+    } else {
+      el.className = "alert alert-danger";
     }
-    this.cards = cards;
   };
 
-  // End match if there is a card selected
-  Hand.prototype.play = function () {
-    if (!this.selected) return;
-    this.match.end();
-  };
-
-  // Returns how many points selected card or first card worth
-  Hand.prototype.points = function () {
-    var card = this.selected || this.cards[0];
-    return card.value;
-  };
-
-  // Remove card from cards available for selection
-  Hand.prototype.removeCard = function () {
-    var index = 0;
-    if (this.selected) index = this.cards.indexOf(this.selected);
-    return this.cards.splice(index, 1);
-  };
-
-  // Reshuffle unused cards back to deck
-  Hand.prototype.reshuffle = function () {
-    if (!this.cards.length) return;
-    var deck = this.deck.cards;
-    this.cards.forEach(function (card) {
-      var index = Math.floor(Math.random() * deck.length);
-      deck.splice(index, 0, card);
+  // Find hand with highest score
+  Game.prototype.findFinalWinner = function () {
+    return this.hands.reduce(function (winner, hand) {
+      if (!winner || hand.score >= winner.score) {
+        return hand;
+      } else {
+        return winner;
+      }
     });
   };
 
-  // Increase score while updating UI
-  Hand.prototype.increaseScore = function () {
-    this.scoreEl.textContent = ++this.score;
+  Game.prototype.displayFinalWinner = function (winner) {
+    var el = find("end-msg");
+    var victory = find("game-victory");
+    var defeat = find("game-defeat");
+
+    el.textContent = winner.id + " has won the game!";
+    if (winner.id === "player") {
+      el.className = "alert alert-success";
+      show(victory);
+      hide(defeat);
+    } else {
+      el.className = "alert alert-danger";
+      show(defeat);
+      hide(victory);
+    }
   };
 
-  // Set selected card and unselect others
-  Hand.prototype.select = function (selected) {
-    this.selected = selected;
-    this.cards.forEach(function (card) {
-      if (selected !== card) card.unselect();
-    });
+  Game.prototype.end = function () {
+    hide(this.stageEl);
+    show(this.endEl);
+    var winner = this.findFinalWinner();
+    this.displayFinalWinner(winner);
   };
 
   function Match(hands) {
@@ -143,24 +124,18 @@
   };
 
   Match.prototype.end = function () {
-    var winner;
-
-    this.hands.forEach(function (hand) {
-      if (!winner) {
-        winner = hand;
-      } else if (winner.points() <= hand.points() ) {
-        winner = hand;
+    var winner = this.hands.reduce(function (winner, hand) {
+      if (!winner || winner.points() <= hand.points()) {
+        return hand;
+      } else {
+        return winner;
       }
-      hand.removeCard();
-      hand.reshuffle();
     });
 
-    if (winner.id === "player") {
-      playAudio("hit");
-    } else {
-      playAudio("miss");
-    }
+    if (winner.id === "player") playAudio("hit");
+    else playAudio("miss");
 
+    this.hands.forEach(function (hand) { hand.reshuffle(); });
     this.endCallback(winner);
   };
 
@@ -221,6 +196,99 @@
     return decks;
   };
 
+  function Hand(options) {
+    this.id = options.id;
+    this.deck = options.deck;
+    this.maxCards = options.maxCards || 1;
+    this.playable = options.playable;
+    this.score = 0;
+    this.cards = null;
+    this.selected = null;
+    this.setEls();
+  }
+
+  // Set current match
+  Hand.prototype.setMatch = function (match) {
+    this.match = match;
+  };
+
+  // Whether hand still has cards to play
+  Hand.prototype.hasCards = function () {
+    return !!this.deck.cards.length;
+  };
+
+  // Set hand dom element
+  Hand.prototype.setEls = function () {
+    var id = this.id;
+    this.el = find(id + "-hand");
+    this.scoreEl = find(id + "-score");
+    this.scoreEl.textContent = 0;
+    if (!this.playable) return;
+    var btn = find(id + "-btn");
+    btn.onclick = this.play.bind(this);
+  };
+
+  // Render max cards for hand and set current cards
+  Hand.prototype.render = function () {
+    var hand = this;
+    this.el.innerHTML = "";
+    var cards = this.deck.deal(this.maxCards);
+    cards.forEach(function (card) {
+      card.setHand(hand);
+      var el = card.render(hand.playable);
+      hand.el.appendChild(el);
+    });
+    this.cards = cards;
+  };
+
+  // End match if there is a card selected
+  Hand.prototype.play = function () {
+    if (!this.selected) return;
+    this.match.end();
+  };
+
+  // Returns how many points selected card or first card worth
+  Hand.prototype.points = function () {
+    var card = this.selected || this.cards[0];
+    return card.value;
+  };
+
+  // Remove card from cards available for selection
+  Hand.prototype.removeCard = function () {
+    var index = 0;
+    if (this.selected) index = this.cards.indexOf(this.selected);
+    this.cards.splice(index, 1);
+  };
+
+  // Reshuffle unused cards back to deck
+  Hand.prototype.reshuffle = function () {
+    this.removeCard();
+    if (!this.cards.length) return;
+    var deck = this.deck.cards;
+    this.cards.forEach(function (card) {
+      var index = Math.floor(Math.random() * deck.length);
+      deck.splice(index, 0, card);
+    });
+    this.selected = null;
+  };
+
+  // Increase score while updating UI
+  Hand.prototype.increaseScore = function () {
+    this.scoreEl.textContent = ++this.score;
+  };
+
+  // Set selected card and unselect others
+  Hand.prototype.select = function (selected) {
+    this.selected = selected;
+    this.cards.forEach(function (card) {
+      if (selected !== card) card.unselect();
+    });
+  };
+
+  Hand.prototype.unselect = function (card) {
+    if (this.selected === card) this.selected = null;
+  };
+
   // Create card element
   function Card(suit, face, value, position) {
     this.suit = suit;
@@ -244,27 +312,46 @@
   };
 
   Card.prototype.select = function () {
-    this.el.className = "selected";
-    this.hand.select(this);
+    if (this.active) {
+      this.unselect();
+    } else {
+      this.active = true;
+      this.hand.select(this);
+      this.el.className = "active";
+    }
   };
 
   Card.prototype.unselect = function () {
+    this.active = false;
+    this.hand.unselect(this);
     this.el.className = "";
   };
 
   function playAudio(id, volume) {
-    var sound = document.querySelector("audio#sound-" + id);
+    var sound = find("sound-" + id);
     sound.volume = volume || 1;
     sound.load();
     sound.play();
   };
 
   function stopAudio(id) {
-    var sound = document.querySelector("audio#sound-" + id);
+    var sound = find("sound-" + id);
     sound.pause();
   }
 
-  var game = new Game();
-  game.start();
+  function hide(el) {
+    el.className += " hidden";
+  }
+
+  function show(el) {
+    el.className = el.className.replace(/hidden/g, "")
+  }
+
+  function find(selector) {
+    return document.getElementById(selector);
+  }
+
+  playAudio("battle", .2);
+  new Game();
 
 }());
